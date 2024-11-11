de505d7c12ef26f7fda4f3f11e854f837a356970
victorac <victorde.ac@gmail.com>
2024-11-11 14:39:26 +0000

    Deploying to gh-pages from @ victorac/acordes@520806c6ed4f858f34098662b90d7d8166bb8f7b 🚀

---
+++ _next/static/chunks/app/layout-542895ba990d836c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6830:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1571,23)),Promise.resolve().then(l.t.bind(l,5214,23))},5214:()=>{},1571:e=>{e.exports={style:{fontFamily:"'Nunito', 'Nunito Fallback'",fontStyle:"normal"},className:"__className_6db340"}}},e=>{var s=s=>e(e.s=s);e.O(0,[547,130,215,744],()=>s(6830)),_N_E=e.O()}]);
